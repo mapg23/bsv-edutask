041c8ca09ee20d22bcc796efbc64f4d1434b300b
mapg23 <mapg23@student.bth.se>
2025-04-18 16:22:25 +0200

    tests

--- backend/test/test_userController.py
+++ backend/test/test_userController.py
@@ -1,10 +1,10 @@
 import pytest
-import unittest.mock as mock
-from unittest.mock import patch
+from unittest.mock import MagicMock
 
 from src.controllers.usercontroller import UserController
 
 import re
+
 
 class TestUserController:
     """
@@ -12,29 +12,40 @@ class TestUserController:
     """
 
     @pytest.fixture
-    def mock_dao():
-        return mock.MagicMock()
+    def mock_dao(self):
+        return MagicMock()
 
     @pytest.fixture
-    def controller(mock_dao):
+    def controller(self, mock_dao):
         return UserController(dao=mock_dao)
 
-    def test_valid_email_and_user():
+    def test_valid_email_and_user(self):
         ""
         ""
 
-    def test_invalid_email_with_user():
+    def test_invalid_email_with_user(self):
         ""
         ""
 
-    def test_valid_email_with_multiple_user():
+    def test_valid_email_with_multiple_user(self):
         ""
         ""
 
-    def test_valid_email_with_no_user():
-        ""
-        ""
+    def test_valid_email_with_no_user(self, controller, mock_dao):
+        email = 'username@email.com'
 
-    def test_invalid_email_and_user():
-        ""
-        ""
+        mock_dao.find.return_value = []
+
+        try:
+            user = controller.get_user_by_email(email)
+        except IndexError as index_error_msg:
+            assert str(index_error_msg) == 'list index out of range'
+
+        mock_dao.find.assert_called_once_with({'email': email})
+
+    def test_invalid_email_and_user(self, controller):
+        email = 'invalid'
+        try:
+            controller.get_user_by_email(email)
+        except ValueError as error_msg:
+            assert str(error_msg) == 'Error: invalid email address'
