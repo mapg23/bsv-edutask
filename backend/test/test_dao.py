4e14fa1de20311e6f61586c07d5477c7478bdbcc
mapg23 <mapg23@student.bth.se>
2025-04-24 16:14:12 +0200

    method added

--- backend/test/test_dao.py
+++ backend/test/test_dao.py
@@ -15,3 +15,12 @@ class TestUserController:
         mock_inserted_id = ObjectId()
 
         collection.insert_one.return_value.inserted_id = mock_inserted_id
+
+
+    def test_create_valid_user(self):
+
+    def test_create_user_invalid_type(self):
+    
+    def test_create_user_invalid_duplicate(self):
+
+    def test_create_user_missing_field(self):
