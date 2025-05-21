4bc80a273b8b73daf7789095e6459bdd129d103b
mapg23 <mapg23@student.bth.se>
2025-05-21 10:01:43 +0200

    fixed test_dao.py

--- backend/test/test_dao.py
+++ backend/test/test_dao.py
@@ -2,6 +2,7 @@ import pytest
 from unittest.mock import MagicMock, patch
 from bson import ObjectId
 from src.util.dao import DAO
+from dotenv import load_dotenv
 
 from pymongo.errors import WriteError, DuplicateKeyError
 
@@ -11,45 +12,33 @@ class TestDAO:
     Test for DAO.
     """
 
-    @pytest.fixture
-    def mock_dao(self):
-        # Patch MongoClient so it doesn't actually connect to MongoDB
-        with patch('src.util.dao.pymongo.MongoClient') as mock_mongo_client:
-        # Create a fake collection and set up its behavior
-            mock_collection = MagicMock()
-            inserted_id = ObjectId()
+    @pytest.fixture(scope="function")
+    def mock_dao(self, monkeypatch):
+        load_dotenv()
+        monkeypatch.setenv("MONGO_URL", "mongodb://root:root@localhost:27017")
 
-            mock_collection.insert_one.return_value.inserted_id = inserted_id
-            mock_collection.find_one.return_value = {
-                '_id': inserted_id,
-                'name': 'Mocked Name',
-                'active': True
-            }
+        dao = DAO("user")
+        dao.drop()
+        dao = DAO("user")
 
-            # Mock the database and collection access
-            mock_db = MagicMock()
-            mock_db.list_collection_names.return_value = ['testcollection']
-            mock_db.__getitem__.return_value = mock_collection
+        yield dao
 
-            mock_mongo_client.return_value = MagicMock()
-            mock_mongo_client.return_value.edutask = mock_db
+        dao.drop()
 
-            dao = DAO(collection_name='testcollection')
-
-            return dao
 
     def test_create_valid_user(self, mock_dao):
         """
         Test with the right output
         """
-        input_data = {'name': 'Mocked Name', 'active': True}
+        input_data = {'firstName': 'testFirstname', 'lastName' : 'testLastName', 'email': 'name@email.com'}
 
         result = mock_dao.create(input_data)
 
         assert result == {
             '_id' : result['_id'],
-            'name': 'Mocked Name',
-            'active': True
+            'firstName': input_data['firstName'],
+            'lastName': input_data['lastName'],
+            'email': input_data['email']
         }
 
     def test_create_user_invalid_type(self, mock_dao):
@@ -57,10 +46,8 @@ class TestDAO:
         Tests wrong input type, bool instead of string.
         """
 
-        invalid_data = {'name': False, 'active': 'Wrong'}
+        invalid_data = {'firstName': False, 'lastName': 10, 'email': None}
 
-        mock_dao.collection.insert_one.side_effect = WriteError("Invalid type")
-        
         with pytest.raises(WriteError):
             mock_dao.create(invalid_data)
     
@@ -68,9 +55,12 @@ class TestDAO:
         """
         Tests if the users input already exists in the database.
         """
-        input_data = {'name': 'mocked name', 'active': True}
+        mock_dao.collection.create_index('email', unique=True)
 
-        mock_dao.collection.insert_one.side_effect = DuplicateKeyError("A duplicate key error occurred")
+        input_data = {'firstName': 'testFirstname', 'lastName' : 'testLastName', 'email': 'name@email.com'}
+
+        # creates first entry of same data
+        mock_dao.create(input_data)
 
         with pytest.raises(DuplicateKeyError):
             mock_dao.create(input_data)
@@ -79,9 +69,9 @@ class TestDAO:
         """
         Tests if the users input does not contain all the required properties.
         """
-        input_data = {'active': True}
+        input_data = {'firstName': 'testFirstname'}
 
-        mock_dao.collection.insert_one.side_effect = WriteError("Field 'name' is required")
+        # mock_dao.collection.insert_one.side_effect = WriteError("Field 'name' is required")
 
         with pytest.raises(WriteError):
             mock_dao.create(input_data)
